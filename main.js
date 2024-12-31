cb3d84ba3b5d409682da5f87616d4bfec90d1e08
Vic Pues <141432283+vicpues@users.noreply.github.com>
2024-12-31 15:32:24 +0100

    test: Add final rebalancing test

--- main.js
+++ main.js
@@ -1,10 +1,39 @@
 const BinaryTree = require("./binary-tree");
 
-const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const array = [];
+while (array.length < 20) array.push(Math.floor(Math.random() * 100));
 const tree = new BinaryTree(array);
 
 tree.print();
-console.log(tree.has(7));
-tree.remove(7);
+console.log(tree.isBalanced()); // true
+
+allOrders(); // Last printout should be all the values from low to high.
+
+const newValues = [];
+while (newValues.length < 6)
+    newValues.push(Math.floor(Math.random() * 100) + 100);
+newValues.forEach((value) => tree.insert(value));
 tree.print();
-console.log(tree.has(7));
+console.log(tree.isBalanced()); // false
+
+tree.rebalance();
+tree.print();
+console.log(tree.isBalanced()); // true
+console.log();
+
+allOrders(); // Last printout should be all the values from low to high.
+
+/** Prints every value in the tree in level order, then preorder, then
+ * postorder, and finally inorder.
+ */
+function allOrders() {
+    const printValue = (value) => console.log(value);
+    tree.levelOrder(printValue);
+    console.log();
+    tree.preOrder(printValue);
+    console.log();
+    tree.postOrder(printValue);
+    console.log();
+    tree.inOrder(printValue); // All the nodes from low to high
+    console.log();
+}
